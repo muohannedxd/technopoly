6306722f1d00f0bb7d8db3126f077c62a9929fbe
iimeeeed <fdmn740@gmail.com>
2024-12-28 19:13:38 +0100

    Initial agenda frame

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,14 @@ import { Footer } from '@/components/Footer'
 import { Hero } from '@/components/Hero'
 import { Navbar } from '@/components/Navbar'
 import { ScrollToTop } from '@/components/ScrollToTop'
+import { Agenda } from '@/components/Agenda'
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Agenda/>
       <FAQ />
       <Footer />
       <ScrollToTop />
