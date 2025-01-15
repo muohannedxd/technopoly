2f4e2737d58eb78619b37c30fb143cc08cace592
muohannedxd <mohanned.kadache@ensia.edu.dz>
2025-01-15 01:42:32 +0100

    added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,26 +17,32 @@ export const Footer = () => {
         </div>
 
         {/** GDG Coin */}
-        <div className="absolute left-6 md:left-12 top-10">
+        <div className="absolute left-6 md:left-12 top-10 hover:scale-105 transition-all duration-300">
           <img src={GDGCoin} alt="GDGCoin" className="w-32 sm:w-44 lg:w-56" />
         </div>
 
         <div className="flex pt-20 pb-12 gap-12 md:gap-20 items-center">
-          <Link to="mailto:gdg@ensia.edu.dz" className="w-10 md:w-16">
+          <Link
+            to="mailto:gdg@ensia.edu.dz"
+            className="w-10 md:w-16 hover:scale-105 transition-all duration-300"
+          >
             <img src={Gmail} alt="Gmail" />
           </Link>
-          <Link to="https://discord.gg/Tk8xG3T8" className="w-10 md:w-16">
+          <Link
+            to="https://discord.gg/Tk8xG3T8"
+            className="w-10 md:w-16 hover:scale-105 transition-all duration-300"
+          >
             <img src={Discord} alt="Discord" />
           </Link>
           <Link
             to="https://www.instagram.com/gdg_ensia/"
-            className="w-10 md:w-16"
+            className="w-10 md:w-16 hover:scale-105 transition-all duration-300"
           >
             <img src={Instagram} alt="Instagram" />
           </Link>
           <Link
             to="https://www.linkedin.com/company/gdg-ensia/"
-            className="w-10 md:w-16"
+            className="w-10 md:w-16 hover:scale-105 transition-all duration-300"
           >
             <img src={LinkedIn} alt="LinkedIn" />
           </Link>
