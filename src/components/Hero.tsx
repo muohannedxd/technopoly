dc198446e2e3f59d291ab8aa843ff1dce7195204
muohannedxd <mohanned.kadache@ensia.edu.dz>
2025-01-30 22:14:51 +0100

    added dice loader

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,9 +4,12 @@ import HeroMobile from "@/assets/images/hero-mobile.svg";
 import TimeDate from "./Hero/TimeDate";
 import Location from "./Hero/Location";
 import { HashLink } from "react-router-hash-link";
+import RollingDice from "@/assets/icons/rolling-dice.gif"
+
 
 export const Hero = () => {
   const [isMobile, setIsMobile] = useState(false);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
 
   const checkIsMobile = () => {
     setIsMobile(window.innerWidth <= 768);
@@ -19,25 +22,38 @@ export const Hero = () => {
   }, []);
 
   return (
-    <div className="relative bg-white p-2 md:p-8">
-      <img
-        src={isMobile ? HeroMobile : HeroImage}
-        alt="GDG Technopoly"
-        className={`${!isMobile && ""} w-full`}
-      />
-      <div className="absolute top-[30%] md:top-[36%] left-1/2 md:left-2/3 transform -translate-x-1/2 -translate-y-1/2">
-        <TimeDate />
-      </div>
-      <div className="absolute top-[70%] md:top-[62%] left-1/2 md:left-1/3 transform -translate-x-1/2 -translate-y-1/2">
-        <Location />
-      </div>
-      <HashLink
-        rel="noreferrer noopener"
-        to="/register"
-        className="bg-blue-300 absolute bottom-4 md:bottom-8 left-1/2 -translate-x-[57%] md:-translate-x-[76%] h-[12%] md:h-[18%] w-[39%] md:w-[20%] opacity-0"
+    <>
+      {!isImageLoaded && (
+        <div className="w-full h-screen flex items-center justify-center bg-slate-50">
+          <img src={RollingDice} alt="Loading" className="w-20 h-20 md:w-24 md:h-24" />
+        </div>
+      )}
+
+      <div
+        className={`relative bg-white p-2 md:p-8 ${
+          !isImageLoaded ? "hidden" : ""
+        }`}
       >
-        REGISTER
-      </HashLink>
-    </div>
+        <img
+          src={isMobile ? HeroMobile : HeroImage}
+          alt="GDG Technopoly"
+          className="w-full"
+          onLoad={() => setIsImageLoaded(true)}
+        />
+        <div className="absolute top-[30%] md:top-[36%] left-1/2 md:left-2/3 transform -translate-x-1/2 -translate-y-1/2">
+          <TimeDate />
+        </div>
+        <div className="absolute top-[70%] md:top-[62%] left-1/2 md:left-1/3 transform -translate-x-1/2 -translate-y-1/2">
+          <Location />
+        </div>
+        <HashLink
+          rel="noreferrer noopener"
+          to="/register"
+          className="bg-blue-300 absolute bottom-4 md:bottom-8 left-1/2 -translate-x-[57%] md:-translate-x-[76%] h-[12%] md:h-[18%] w-[39%] md:w-[20%] opacity-0"
+        >
+          REGISTER
+        </HashLink>
+      </div>
+    </>
   );
 };
